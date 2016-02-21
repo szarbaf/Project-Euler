ec46bce2aeef933b01fd93710f325c494db799e0
Shahrooz Zarbafian <szarbaf@bu.edu>
2016-02-20 23:02:42 -0500

    Finished 4.

--- 4.cpp
+++ 4.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
+bool palindrom(int num);
+
 int main(){
-		
-		int max_num = 0;
-		for (int i = 100; i <= 999; i++)
-				for(int j = i; i <= 999 j++){
-						int num = i*j;
-						if (palindrom(num))
-								max_num = max(num, max_num);
-				}
 
-		cout << "The maximum palindrom number is : " << max_num << endl;
+	int max_num = 0;
+	for (int i = 999; i >= 100; i--)
+		for(int j = i; j >= 100 ; j--){
+			int num = i*j;
+			if (palindrom(num)){
+				max_num = max(max_num, num);
+			}
+		}
 
-		return 0;
+	cout << "The maximum palindrom number is : " << max_num << endl;
+
+	return 0;
 }
 
 bool palindrom(int num){
-		string st = to_string(num);
-
-		
+	
+	string st = to_string(num);
+	int s=-1, e=st.size();
+	while (s++ < e--)
+		if(st[s] != st[e])
+			break;
+	
+	if (s < e)
+		return false;
+	else
+		return true;
 }
