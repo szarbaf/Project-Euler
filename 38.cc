ec40b217b2d2c51039d7e603ae3239a61f0af041
Shahrooz Zarbafian <szarbaf@bu.edu>
2018-08-30 17:07:24 -0400

    Started problem 38.

--- 38.cc
+++ 38.cc
@@ -12,12 +12,67 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
 
 using namespace std;
 
+void NextLargestPanDigital(vector<int> &cur_pan_digital){
+
+	// This function returns the next permutation of a pandigital number. Specifically, it modifies the input
+	// to contain largest pandigital number that is smaller than cur_pan_digital.
+
+
+	// Starting from the one to the last digit, finding the highest index i for which there is at least
+	// an index j where 
+	// 1- cur_pan_digital[i] < cur_pan_digital[j] 
+	// 2- j > i, i.e. j \in [i+1, i+2, ..., size-1].
+	// Among all the indices j for a praticular value of i, the j with the highest value of cur_pan_digital[j] is returned.
+	// For example for 15342 the max_index is 3 corresponding to the digit 4.
+	
+	if (cur_pan_digital.size() < 2)
+		return;
+	const int kMaxDigits = cur_pan_digital.size();
+	int i = kMaxDigits - 1, max_index, max_val = INT_MIN;
+	bool found = false;
+	while(i-- >= 0 && !found){
+		for (int j = i+1; j < kMaxDigits; j++)
+			if (cur_pan_digital[i] < cur_pan_digital[j]){
+				found = true;
+				if (cur_pan_digital[j] > max_val){
+					max_val = cur_pan_digital[j];
+					max_index = j;
+				}
+			}
+	}
+
+	if (!found)
+		return;
+
+	// Swapping the values.
+	iter_swap(cur_pan_digital.begin()+i, cur_pan_digital.begin()+max_index);
+
+	// Sorting values of the array from i+1 to the end in a descending order.
+	sort(cur_pan_digital.begin()+i+1, cur_pan_digital.end(), greater<int>()) ;
+
+	return;
+
+	
+}
 
 int main() {
 
+	vector<int> cur_pan_digital = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	while (!IsConcatProduct(cur_pan_digital))
+		NextLargestPanDigital(cur_pan_digital);
 
+	cout << " The largest pan digital as a concatenation form is : ";
+	for (auto d : cur_pan_digital)
+		cout << d;
+	cout << endl;
+
+	return 0;
+	
 
 }
